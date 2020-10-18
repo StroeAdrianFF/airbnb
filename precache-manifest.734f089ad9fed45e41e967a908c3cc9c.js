4b153867bc62ba117cf90ea6a174e48f25a09f93
adrian <stroeadrian85@yahoo.com>
2020-10-18 16:33:03 +0300

    Updates

---
+++ precache-manifest.734f089ad9fed45e41e967a908c3cc9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4df6617a73afc9ee9acc94aae8fc344",
+    "url": "/StroeAdrianFF/airbnb/index.html"
+  },
+  {
+    "revision": "d39e1c63f143c35a59e4",
+    "url": "/StroeAdrianFF/airbnb/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "f3e973934039bc4ea48c",
+    "url": "/StroeAdrianFF/airbnb/static/css/main.65a20abd.chunk.css"
+  },
+  {
+    "revision": "d39e1c63f143c35a59e4",
+    "url": "/StroeAdrianFF/airbnb/static/js/2.3c48b6f1.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/StroeAdrianFF/airbnb/static/js/2.3c48b6f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3e973934039bc4ea48c",
+    "url": "/StroeAdrianFF/airbnb/static/js/main.ec8dae03.chunk.js"
+  },
+  {
+    "revision": "50a28a8e6721c84620ac",
+    "url": "/StroeAdrianFF/airbnb/static/js/runtime-main.5ebaac1e.js"
+  },
+  {
+    "revision": "56c6e390be6fcbfc400048bd9a05bc20",
+    "url": "/StroeAdrianFF/airbnb/static/media/logo.56c6e390.png"
+  }
+]);
